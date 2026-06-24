0603b505e98bdc4c64b348c35aeb5d34385bddc0
Ahmad Said <mail.saydorrahman@gmail.com>
2026-06-24 12:37:40 +0600

    feat: add design system, fonts, and root layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,5 @@
 import dns  from "node:dns";
 dns.setServers(["8.8.8.8", "8.8.4.4"]);
-
 import { Inter } from "next/font/google";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
